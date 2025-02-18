a7e99697635d973b5279ea7366c8116ee38b8bc8
el983 <elpislearningconsultantsllp88@gmail.com>
2025-02-18 17:55:02 +0530

    Add files via upload

---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './OurTeam.css'
+const OurTeam = () => {
+  return (
+    <div className='ourteam'>
+        <p>gdfgdfhhf</p>
+    </div>
+  )
+}
+
+export default OurTeam
